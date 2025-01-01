8b249d43f4678d5cddb86dc5cbbe78aac76504ae
Fadil Sestan <fadil.sestan@gmail.com>
2025-01-01 13:45:52 +0100

    feat: removed walls assets, updates scale of player and game speed

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { generateSpriteSheetInformations } from "../utils/generateSpriteSheetInformations";
 import { intersectsRect } from "../utils/intersects";
 
 interface CanvasProps {
@@ -12,6 +13,19 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
+    const getRandomSx = (variant: "tree" | "path" | "grass" | "logs") => {
+      let options: number[] = [];
+      if (variant === "tree") options = [0, 180, 360];
+
+      if (variant === "path") options = [32, 64];
+
+      if (variant === "logs") options = [0, 128];
+
+      if (variant === "grass") options = fieldGrassTiles;
+
+      return options[Math.floor(Math.random() * options.length)];
+    };
+
     const containerWidth = window.innerWidth;
     const containerHeight = window.innerHeight;
     const viewportWidth = Math.max(containerWidth, 800);
@@ -22,62 +36,167 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    // background field sprite sheet
-    const fieldAssetsSpriteSheet = new Image();
-    fieldAssetsSpriteSheet.src = "/src/assets/assets.png";
+    // Field sprite sheet
+    const {
+      spritesheet: fieldSpriteSheet,
+      spriteWidth: fieldSpriteWidth,
+      spriteHeight: fieldSpriteHeight,
+      totalCols: fieldTotalCols,
+    } = generateSpriteSheetInformations({
+      url: "/src/assets/field.png",
+      spriteWidth: 32,
+      spriteHeight: 32,
+      totalCols: 8,
+    });
 
-    const fieldAssetsWidth = 156;
-    const fieldAssetsHeight = 256;
-    const fieldAssetsSpawnInterval = 400; // Spawn a new tree every 1 second
-    let lastFieldAssetsSpawnTime = 0;
-    const trees: { x: number; y: number; sx: number }[] = [];
-
-    // Function to spawn a new tree
-    const spawnTree = () => {
-      const randomX =
-        Math.random() < 0.5
-          ? Math.random() * minXTree // Spawn left of minX
-          : Math.random() * (window.innerWidth - maxXTree) + maxXTree; // Spawn right of maxX
-      const randomSx = getRandomSx("tree"); // Get a random `sx` value
-      trees.push({ x: randomX, y: -fieldAssetsHeight, sx: randomSx });
-    };
-
-    const fieldWallsSpriteSheet = new Image();
-    fieldWallsSpriteSheet.src = "/src/assets/walls.png";
-
-    const fieldSpriteSheet = new Image();
-    fieldSpriteSheet.src = "/src/assets/field.png";
-
-    const fieldSpriteWidth = 32; // Width of a single player sprite
-    const fieldSpriteHeight = 32; // Height of a single player sprite
-    const fieldTotalCols = 8;
     const fieldGrassTiles = [0, 0, 0, 20, 30, 0, 0, 0];
     const tilesPerRow = viewportWidth / fieldSpriteWidth; // 25 tiles
     const tilesPerCol = viewportHeight / fieldSpriteWidth; // Example 18.75 (~19 tiles)
     const centerTilePerRow = Math.floor(tilesPerRow / 2);
 
-    // Player sprite sheet
-    const playerSpriteSheet = new Image();
-    playerSpriteSheet.src = "/src/assets/player.png";
+    const minXTree = 32 * (centerTilePerRow - 6);
+    const maxXTree = 32 * (centerTilePerRow + 6);
 
-    const playerSpriteWidth = 32; // Width of a single player sprite
-    const playerSpriteHeight = 32; // Height of a single player sprite
-    const playerTotalFrames = 24; // Total frames in the player sprite sheet
+    let lastTimestamp = 0;
+    let accumulatedDistance = 0;
+
+    const {
+      spritesheet: fieldAssetsSpriteSheet,
+      spriteWidth: fieldAssetsWidth,
+      spriteHeight: fieldAssetsHeight,
+      spawnInterval: fieldAssetsSpawnInterval,
+    } = generateSpriteSheetInformations({
+      url: "/src/assets/assets.png",
+      spriteWidth: 156,
+      spriteHeight: 256,
+    });
+
+    // helpers and background assets
+    let lastFieldAssetsSpawnTime = 0;
+    const assets: {
+      x: number;
+      y: number;
+      sx: number;
+      sy: number;
+      sw?: number;
+      sh?: number;
+    }[] = [
+      {
+        x: Math.random() * (window.innerWidth - maxXTree) + maxXTree / 2,
+        y: canvas.height - fieldAssetsHeight * 3,
+        sx: getRandomSx("tree"),
+        sy: 0,
+      },
+      {
+        x: Math.random() * minXTree,
+        y: canvas.height - fieldAssetsHeight * 4,
+        sx: getRandomSx("tree"),
+        sy: 0,
+      },
+    ];
+
+    // Function to spawn a new tree
+    const spawnTree = () => {
+      let randomX =
+        assets.length % 2 === 0
+          ? Math.random() * (window.innerWidth - maxXTree) + maxXTree
+          : Math.random() * minXTree; // Spawn left of minX
+      let randomY =
+        assets.length === 0
+          ? 0
+          : assets[assets.length - 1].y - fieldAssetsHeight;
+      const randomSx = getRandomSx("tree"); // Get a random `sx` value
+      let safeToSpawn = true;
+      // let attemptCount = 0;
+
+      while (!safeToSpawn) {
+        randomX = Math.random() * minXTree;
+
+        safeToSpawn = true;
+        for (const tree of assets) {
+          if (
+            intersectsRect({
+              rect1: {
+                x: randomX,
+                y: randomY,
+                width: fieldAssetsWidth / 3,
+                height: fieldAssetsHeight / 3,
+              },
+              rect2: {
+                x: tree.x,
+                y: tree.y,
+                width: fieldAssetsWidth / 3,
+                height: fieldAssetsHeight / 3,
+              },
+            }) &&
+            randomY > playerY
+          ) {
+            safeToSpawn = false;
+            break;
+          }
+        }
+        // attemptCount++;
+      }
+
+      if (safeToSpawn) {
+        assets.push({
+          x: randomX,
+          y: randomY,
+          sx: randomSx,
+          sy: 0,
+        });
+      }
+      // else {
+      //   assets.push({
+      //     x: newRandomX,
+      //     y: -fieldAssetsHeight,
+      //     sx: getRandomSx("logs"),
+      //     sy: 320,
+      //     sw: 110,
+      //     sh: 156,
+      //   });
+      // }
+    };
+
+    // Player sprite sheet
+    const {
+      spritesheet: playerSpriteSheet,
+      spriteWidth: playerSpriteWidth,
+      spriteHeight: playerSpriteHeight,
+      totalFrames: playerTotalFrames,
+      scale: playerScale,
+      spawnInterval: playerFrameDelay,
+    } = generateSpriteSheetInformations({
+      url: "/src/assets/player.png",
+      spriteWidth: 32,
+      spriteHeight: 32,
+      totalCols: 1,
+      totalRows: 24,
+      scale: 1.25,
+      spawnInterval: 100,
+    });
+
     let playerCurrentFrame = 18; // Current frame of the player animation
     let playerX = 32 * centerTilePerRow - 1; // Player's x position
     let playerY = canvas.height - playerSpriteHeight * 4; // Player's y position
-    const playerScale = 1.5; // Scale factor for the player
-    const playerFrameDelay = 100; // Frame delay in milliseconds
     let lastPlayerFrameTime = 0;
     let playerSpeed = 32; // Speed of the player
 
     // Enemy sprite sheet
-    const enemySpriteSheet = new Image();
-    enemySpriteSheet.src = "/src/assets/enemy.png";
-
-    const enemySpriteWidth = 16; // Width of a single enemy sprite
-    const enemySpriteHeight = 16; // Height of a single enemy sprite
-    const enemyTotalFrames = 8; // Total frames in the enemy sprite sheet
+    const {
+      spritesheet: enemySpriteSheet,
+      spriteWidth: enemySpriteWidth,
+      spriteHeight: enemySpriteHeight,
+      scale: enemyScale,
+      totalFrames: enemyTotalFrames,
+    } = generateSpriteSheetInformations({
+      url: "/src/assets/enemy.png",
+      spriteWidth: 16,
+      spriteHeight: 16,
+      scale: 1.25,
+      totalCols: 1,
+      totalRows: 8,
+    });
     let enemyCurrentFrame = 0; // Current frame of the enemy animation
     const enemyPositions = [
       {
@@ -91,7 +210,6 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
         animationSpeed: 100,
       },
     ];
-    const enemyScale = 2; // Scale factor for enemies
     let lastEnemyFrameTime = 0;
     let lastEnemyAddTime = 0;
     const enemySpeed = 2; // Speed of enemies
@@ -110,7 +228,7 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
       ctx.drawImage(
         image,
         sourceX,
-        0, // Source y (assuming a single row of sprites)
+        0,
         spriteWidth,
         spriteHeight,
         x,
@@ -120,53 +238,37 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
       );
     };
 
-    const getRandomSx = (variant: "tree" | "path" | "grass") => {
-      let options: number[] = [];
-      if (variant === "tree") options = [0, 180, 360];
-
-      if (variant === "path") options = [32, 64];
-
-      if (variant === "grass") options = fieldGrassTiles;
-
-      return options[Math.floor(Math.random() * options.length)];
-    };
-
     const tilemap = Array.from({ length: tilesPerCol }, () =>
       Array.from({ length: tilesPerRow }, (_, i) =>
         i >= centerTilePerRow - 2 && i <= centerTilePerRow + 2
-          ? getRandomSx("path")
+          ? 32
           : getRandomSx("grass"),
       ),
     );
 
-    const minXTree = 32 * (centerTilePerRow - 6);
-    const maxXTree = 32 * (centerTilePerRow + 6);
     // Animation loop
-
-    let lastTimestamp = 0;
-    let accumulatedDistance = 0;
-
     const animate = (timestamp: number) => {
       if (!ctx) return;
 
-      // Time delta for smooth animation
+      // Time delta for smooth animation of map
       const deltaTime = timestamp - lastTimestamp;
       lastTimestamp = timestamp;
 
-      // Control the speed (adjust this value to slow it down)
-      const speed = 90; // pixels per second
+      // Control the speed of map (adjust this value to slow it down)
+      const speed = 98; // pixels per second
       const distanceToMove = (deltaTime / 1000) * speed;
 
-      // Accumulate the distance
+      // Accumulate the distance of map
       accumulatedDistance += distanceToMove;
 
       // Clear the canvas
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      // Update and draw the tilemap
+      // Update and draw the map
       for (let row = 0; row < tilemap.length; row++) {
         for (let col = 0; col < tilemap[row].length; col++) {
           const tileIndex = tilemap[row][col];
+          if (tileIndex === 0) continue;
           const spriteX = (tileIndex % fieldTotalCols) * fieldSpriteWidth;
           const spriteY =
             Math.floor(tileIndex / fieldTotalCols) * fieldSpriteHeight;
@@ -195,7 +297,11 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
         // Prepend a new row at the top
         const newRow = Array.from({ length: tilesPerRow }, (_, i) =>
           i >= centerTilePerRow - 2 && i <= centerTilePerRow + 2
-            ? getRandomSx("path")
+            ? i >= centerTilePerRow - 1 && i <= centerTilePerRow + 1
+              ? 32
+              : Array.from({ length: 31 }, (_, i) => i + 32)[
+                  Math.floor(Math.random() * 31)
+                ]
             : getRandomSx("grass"),
         );
         tilemap.unshift(newRow);
@@ -229,14 +335,14 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
         );
       });
 
-      // Add a new enemy every half a second
+      // Add a new coin every half a second
       if (timestamp - lastEnemyAddTime > 500 && enemyPositions.length < 10) {
         enemyPositions.push({
           x: [
-            (centerTilePerRow - 2) * 34,
-            (centerTilePerRow - 1) * 34,
+            (centerTilePerRow - 2.5) * 34,
+            (centerTilePerRow - 1.5) * 34,
             centerTilePerRow * 32,
-            (centerTilePerRow - 1) * 34,
+            (centerTilePerRow + 0.5) * 34,
           ][Math.floor(Math.random() * 4)],
           y: -enemySpriteHeight * 1.5,
           animationSpeed: 100, // Update animation frame every 100ms
@@ -244,28 +350,28 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
         lastEnemyAddTime = timestamp;
       }
 
-      // Update and draw trees
-      trees.forEach((tree, index) => {
-        tree.y += 1.5; // Move tree downward
+      // Update and draw assets
+      assets.forEach((asset, index) => {
+        asset.y += 1.65; // Move tree downward
 
-        // Only draw trees that fall within the restricted range
-        if (tree.x < minXTree || tree.x > maxXTree) {
+        // Only draw assets that fall within the restricted range
+        if (asset.x < minXTree || asset.x > maxXTree) {
           ctx.drawImage(
             fieldAssetsSpriteSheet,
-            tree.sx,
-            0,
-            fieldAssetsWidth,
-            fieldAssetsHeight,
-            tree.x,
-            tree.y,
-            fieldAssetsWidth,
-            fieldAssetsHeight,
+            asset.sx,
+            asset.sy,
+            asset.sw || fieldAssetsWidth,
+            asset.sh || fieldAssetsHeight,
+            asset.x,
+            asset.y,
+            asset.sw || fieldAssetsWidth,
+            asset.sh || fieldAssetsHeight,
           );
         }
 
         // Remove tree if it goes offscreen
-        if (tree.y > canvas.height) {
-          trees.splice(index, 1);
+        if (asset.y > canvas.height + 256) {
+          assets.splice(index, 1);
         }
       });
 
@@ -309,14 +415,14 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
           rect1: {
             x: playerX,
             y: playerY,
-            width: playerSpriteWidth,
-            height: playerSpriteHeight,
+            width: playerSpriteWidth * playerScale,
+            height: playerSpriteHeight * playerScale,
           },
           rect2: {
             x: enemy.x,
             y: enemy.y,
-            width: enemySpriteWidth,
-            height: enemySpriteHeight,
+            width: enemySpriteWidth * enemyScale,
+            height: enemySpriteHeight * enemyScale,
           },
         });
 
@@ -341,7 +447,7 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
     const handleKeyDown = (event: KeyboardEvent) => {
       switch (event.key) {
         case "ArrowLeft":
-          if (playerX > (centerTilePerRow - 1) * 32) {
+          if (playerX > (centerTilePerRow - 2) * 32) {
             playerX -= playerSpeed;
           }
           break;
@@ -358,6 +464,8 @@ const Canvas: React.FC<CanvasProps> = ({ score, setScore }) => {
     Promise.all([
       new Promise((resolve) => (playerSpriteSheet.onload = resolve)),
       new Promise((resolve) => (enemySpriteSheet.onload = resolve)),
+      new Promise((resolve) => (fieldAssetsSpriteSheet.onload = resolve)),
+      new Promise((resolve) => (fieldSpriteSheet.onload = resolve)),
     ]).then(() => {
       requestAnimationFrame(animate);
     });
